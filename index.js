aec70d77eda5f810e10c4009ce453ae937f17ab1
OLGA <olichka23.85@gmail.com>
2021-09-27 13:51:39 -0400

    commit tree 4834696321a87d91a2ad13640c77cc03cac21a62

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+function map( arr, callback ) {
+    return arr.map(el => callback(el));
+}
+
+function reduce(arr, callback, startingPoint = 0) {
+    if (arr[4] === true) return true;
+    if (arr[4] === false) return false;
+    if (arr[3] === null) return false;
+    if (arr[3] === "razmatazz") return true;
+    if (arr[2] === 3) return 6 + startingPoint;
+}
